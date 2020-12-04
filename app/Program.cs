3fac2e8edf175cbe2545619915d81b4388fe6607
Sven <svenwerner1997@gmail.com>
2020-12-04 14:45:16 +0100

    Removed samples

--- app/Program.cs
+++ app/Program.cs
@@ -6,22 +6,6 @@ namespace app {
         static void Main(string[] args) {
 
             Console.WriteLine("Hello World!");
-            Aufgabe02();
-        }
-
-
-
-        private static void Aufgabe02() {
-
-            var a = 30;
-
-            for (var i = 1; i < a; i++) {
-                var mod = a % i;
-
-                if (mod == 0) {
-                    Console.WriteLine($"{i} / {a} kann geteilt werden.");
-                }
-            }
         }
     }
 }
